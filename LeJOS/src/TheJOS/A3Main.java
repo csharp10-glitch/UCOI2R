09299a468e2358d57841b694913a4538098525bd
csharp10-glitch <csharp10@uco.edu>
2021-10-23 16:09:57 -0500

    Cao my man, for soothe.

--- LeJOS/src/TheJOS/A3Main.java
+++ LeJOS/src/TheJOS/A3Main.java
@@ -2,12 +2,23 @@ package TheJOS;
 
 import lejos.hardware.Button;
 import lejos.hardware.lcd.LCD;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.SensorMode;
 import lejos.robotics.Color;
+import lejos.utility.Delay;
 
 public class A3Main {
 	// User Configs - Will need to tweak during troubleshooting
-	static final int GO_LINE_COLOR = Color.YELLOW;
-	static final int STOP_COLOR = Color.RED;
+	static final int GO_LINE_COLOR1 = 1;
+	static final int GO_LINE_COLOR2 = 13;
+	static final int GO_LINE_COLOR3 = Color.BLUE;
+	static final int GO_LINE_COLOR4 = Color.BLUE;
+	static final int STOP_COLOR1 = Color.RED;
+	static final int STOP_COLOR2 = 0;
+	static final int STOP_COLOR3 = Color.RED;
+	static final int STOP_COLOR4 = Color.RED;
+	
 	static final double PILOT_LINEAR_SPEED = 20; // m/s
 	static final double PILOT_ANGULAR_SPEED = 10; // deg/s
 	static final double OFFSET = 20.32 * 0.8 * 0.5; // m, 1/2 track width times a correction modifier
@@ -16,33 +27,38 @@ public class A3Main {
 	static final double TURN_INCREMENT = 5; // deg
 
 	public static void main(String[] args) {
+		EV3ColorSensor colourBlue = new EV3ColorSensor(SensorPort.S1);
 		MyPilot pilot = new MyPilot(OFFSET, PILOT_LINEAR_SPEED, PILOT_ANGULAR_SPEED);
 
+
 		// check color at starting point. Prompt for help if not correct color
-		boolean lineDetected = pilot.detectColor(GO_LINE_COLOR);
+		boolean lineDetected = (pilot.detectColor(GO_LINE_COLOR1, colourBlue) | pilot.detectColor(GO_LINE_COLOR2, colourBlue));
 		if (!lineDetected) {
-			helpPilot(pilot, lineDetected);
+			helpPilot(pilot, lineDetected, colourBlue);
+
 		}
 
 		while (true) {
 			pilot.travel(FWD_INCREMENT);
 
 			// stop moving if detects stop color
-			if (pilot.detectColor(STOP_COLOR)) {
+			if (pilot.detectColor(STOP_COLOR1, colourBlue) | pilot.detectColor(STOP_COLOR2, colourBlue)) {
 				break;
 			}
 
-			lineDetected = pilot.detectColor(GO_LINE_COLOR);
+			lineDetected = (pilot.detectColor(GO_LINE_COLOR1, colourBlue) | pilot.detectColor(GO_LINE_COLOR2, colourBlue));
 			if (!lineDetected) {
-				lineDetected = pilot.checkLineLeft(MAX_TURN_ANGLE, TURN_INCREMENT, GO_LINE_COLOR);
+				lineDetected = (pilot.checkLineLeft(MAX_TURN_ANGLE, TURN_INCREMENT, GO_LINE_COLOR1, colourBlue)|pilot.checkLineLeft(MAX_TURN_ANGLE, TURN_INCREMENT, GO_LINE_COLOR1, colourBlue));
 				if (!lineDetected) {
 					// reset to prior position
 					pilot.rotate(MAX_TURN_ANGLE);
-					lineDetected = pilot.checkLineRight(MAX_TURN_ANGLE, TURN_INCREMENT, GO_LINE_COLOR);
+					lineDetected = (pilot.checkLineRight(MAX_TURN_ANGLE, TURN_INCREMENT, GO_LINE_COLOR1, colourBlue)|pilot.checkLineRight(MAX_TURN_ANGLE, TURN_INCREMENT, GO_LINE_COLOR1, colourBlue));
 
 					// if line still not found on right, wait for user to reposition
 					if (!lineDetected) {
-						helpPilot(pilot, lineDetected);
+						helpPilot(pilot, lineDetected, colourBlue);
+						Delay.msDelay(5000);
+						break;
 					}
 				}
 			}
@@ -52,11 +68,12 @@ public class A3Main {
 	/**
 	 * Pilot needs help repositioning from user.
 	 */
-	public static void helpPilot(MyPilot pilot, boolean lineDetected) {
+	public static void helpPilot(MyPilot pilot, boolean lineDetected, EV3ColorSensor colourBlue) {
 		while (!lineDetected) {
 			LCD.drawString("HELP! I'm LOST!", 0, 3);
 			Button.ENTER.waitForPressAndRelease();
-			lineDetected = pilot.detectColor(GO_LINE_COLOR);
+			lineDetected = (pilot.detectColor(GO_LINE_COLOR1, colourBlue) | pilot.detectColor(GO_LINE_COLOR2, colourBlue));
+
 		}
 	}
 }
--- LeJOS/src/TheJOS/MyPilot.java
+++ LeJOS/src/TheJOS/MyPilot.java
@@ -4,6 +4,7 @@ import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.SensorMode;
 import lejos.robotics.chassis.Chassis;
 import lejos.robotics.chassis.Wheel;
 import lejos.robotics.chassis.WheeledChassis;
@@ -11,8 +12,6 @@ import lejos.robotics.navigation.MoveController;
 import lejos.robotics.navigation.MovePilot;
 
 public class MyPilot extends MovePilot{
-	EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S1);
-
 	public MyPilot(double wheelOffset, double linearSpeed, double angularSpeed) {
 		super(initChassis(wheelOffset));
 		setLinearSpeed(linearSpeed);
@@ -21,8 +20,8 @@ public class MyPilot extends MovePilot{
 
 	static Chassis initChassis(double wheelOffset) {
 		double wheelDiam = MoveController.WHEEL_SIZE_NXT1;
-		EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.C);
-		EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(MotorPort.B);
+		EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.D);
+		EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(MotorPort.A);
 		Wheel leftWheel = WheeledChassis.modelWheel(leftMotor, wheelDiam).offset(-1*wheelOffset);
 		Wheel rightWheel = WheeledChassis.modelWheel(rightMotor, wheelDiam).offset(wheelOffset);
 		return new WheeledChassis(new Wheel[] { leftWheel, rightWheel }, WheeledChassis.TYPE_DIFFERENTIAL);
@@ -32,10 +31,10 @@ public class MyPilot extends MovePilot{
 	 * Rotate left slowly and check for line.
 	 * @return true if line detected
 	 */
-	public boolean checkLineLeft(double maxAngle, double increment, int lineColor) {
+	public boolean checkLineLeft(double maxAngle, double increment, int lineColor, EV3ColorSensor colourBlue) {
 		for (double angleTurned = 0; angleTurned > -maxAngle; angleTurned -= increment) {
 			rotate(angleTurned);
-			if(detectColor(lineColor)) {
+			if(detectColor(lineColor, colourBlue)) {
 				return true;
 			}
 		}
@@ -46,17 +45,21 @@ public class MyPilot extends MovePilot{
 	 * Rotate right slowly and check for line.
 	 * @return true if line detected
 	 */
-	public boolean checkLineRight(double maxAngle, double increment, int lineColor) {
+	public boolean checkLineRight(double maxAngle, double increment, int lineColor, EV3ColorSensor colourBlue) {
 		for (double angleTurned = 0; angleTurned < maxAngle; angleTurned += increment) {
 			rotate(angleTurned);
-			if(detectColor(lineColor)) {
+			if(detectColor(lineColor, colourBlue)) {
 				return true;
 			}
 		}
 		return false;
 	}
 	
-	public boolean detectColor(int color) {
-		return colorSensor.getColorID() == color;
+	public boolean detectColor(int color, EV3ColorSensor colourBlue) {
+		SensorMode colourID = colourBlue.getMode("ColorID");
+		float[] colourSample = new float[colourBlue.getColorID()];
+		colourID.fetchSample(colourSample,0);
+		int colour = (int) colourSample[0]; 
+		return colour == color;
 	}
 }
