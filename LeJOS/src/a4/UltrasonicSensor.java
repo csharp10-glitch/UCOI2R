dfb46abf789d6bcfbab55d18d04612f241447089
Cao Truong <ctruong3@uco.edu>
2021-11-07 17:03:45 -0600

    chris s. wall encounter exception

--- LeJOS/src/a4/UltrasonicSensor.java
+++ LeJOS/src/a4/UltrasonicSensor.java
@@ -1,34 +1,48 @@
 package a4;
 
-import lejos.hardware.motor.EV3MediumRegulatedMotor;
 import lejos.hardware.port.Port;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.robotics.SampleProvider;
-import lejos.robotics.filter.MeanFilter;
 
 public class UltrasonicSensor {
-	EV3UltrasonicSensor sensor;
-	SampleProvider distMode;
-	SampleProvider average;
-	int distance;
-	float[] sample;
+    EV3UltrasonicSensor sensor;
+    SampleProvider distMode;
+    SampleProvider average;
+    int distance;
+    float[] sample;
+    private final int DISTANCE_THRESHOLD = 0;
 
-	public UltrasonicSensor(Port s1) {
-		sensor = new EV3UltrasonicSensor(s1);
-		distMode = sensor.getMode("Distance");
-		distance = 255;
-//		average = new MeanFilter(distMode, 5);
-//		sample = new float[average.sampleSize()];
-//		average.fetchSample(sample, 0);
-	}
+    public UltrasonicSensor(Port s1) {
+        sensor = new EV3UltrasonicSensor(s1);
+        distMode = sensor.getMode("Distance");
+        distance = 255;
+//        average = new MeanFilter(distMode, 5);
+//        sample = new float[average.sampleSize()];
+//        average.fetchSample(sample, 0);
+    }
 
-	public int distance() {
-		float [] sample = new float[distMode.sampleSize()];
-		distMode.fetchSample(sample, 0);
-		distance = (int)(sample[0]*100);
-		System.out.println(distance);
-		return distance;
-	}
+    public int distance() {
+        float [] sample = new float[distMode.sampleSize()];
+        distMode.fetchSample(sample, 0);
+        distance = (int)(sample[0]*100);
+        System.out.println(distance);
+        return distance;
+    }
 
-//	ir.close();
+    public void checkForWall() throws WallEncounteredException
+    {
+        if(distance() <= DISTANCE_THRESHOLD)
+        {
+            throw new WallEncounteredException("Wall Encountered!");
+        }
+    }
+
+//    ir.close();
+    public class WallEncounteredException extends Exception
+    {
+        public WallEncounteredException(String errorMessage)
+        {
+            super(errorMessage);
+        }
+    }
 }
