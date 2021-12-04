283d1b91abee8a445c2b7f9f4f763bf322b61f90
Cao Truong <ctruong3@uco.edu>
2021-12-03 19:12:20 -0600

    implemented retrace and exit plan

--- LeJOS/src/finalProject/MazeNode.java
+++ LeJOS/src/finalProject/MazeNode.java
@@ -121,4 +121,29 @@ public class MazeNode {
 	public String toString() {
 		return String.format("Node: (%f,%f), Visited: %b", x,y,visited);
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Float.floatToIntBits(x);
+		result = prime * result + Float.floatToIntBits(y);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MazeNode other = (MazeNode) obj;
+		if (Float.floatToIntBits(x) != Float.floatToIntBits(other.x))
+			return false;
+		if (Float.floatToIntBits(y) != Float.floatToIntBits(other.y))
+			return false;
+		return true;
+	}
 }
---
+++ LeJOS/src/finalProject/TestEscape.java
@@ -0,0 +1,53 @@
+package finalProject;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Iterator;
+
+public class TestEscape {
+	static Deque<Character> nodeStack = new ArrayDeque<>();
+	static Deque<Character> exitPlan = new ArrayDeque<>();
+
+	/**
+	 * exitPlan: (top) O (exit) N J F B A (bottom) 
+	 * nodeStack: (top) P (ball) L K J F B A (bottom) 
+	 * result: P L K J N O
+	 */
+	public static void main(String[] args) {
+		char[] exitCharArr = "ONJFBA".toCharArray();
+		for (int i = exitCharArr.length - 1; i >= 0; i--) {
+			exitPlan.push(exitCharArr[i]);
+		}
+
+		char[] nodeCharArr = "PLKJFBA".toCharArray();
+		for (int i = nodeCharArr.length - 1; i >= 0; i--) {
+			nodeStack.push(nodeCharArr[i]);
+		}
+
+		System.out.println(exitPlan);
+		System.out.println(nodeStack);
+
+		Iterator<Character> exitIt = exitPlan.descendingIterator();
+		Iterator<Character> nodeIt = nodeStack.descendingIterator();
+		Character common = null;
+		while (exitIt.hasNext()) {
+			Character exitNode = exitIt.next();
+			Character node = nodeIt.next();
+			if (exitNode.equals(node)) {
+				common = exitNode;
+				exitPlan.removeLast();
+				nodeStack.removeLast();
+			}
+		}
+
+		if (common != null) {
+			exitPlan.addLast(common);
+		}
+		while (!exitPlan.isEmpty()) {
+			nodeStack.addLast(exitPlan.removeLast());
+		}
+		System.out.println(exitPlan);
+		System.out.println(nodeStack);
+	}
+
+}
